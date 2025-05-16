1cb002500000febdc1065c755dfae76b53f2cce3
Marianehrg <marianeaharag@gmail.com>
2025-05-16 11:27:48 +0200

    adding api on auth route

--- routes/auth.php
+++ routes/auth.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("auth")->group(function () {
+Route::prefix("api/auth")->group(function () {
     Route::post("/signup", [AuthController::class, "signUp"]);
     Route::post("/signin", [AuthController::class, "signIn"]);
     Route::get("/signout", [AuthController::class, "signOut"])->middleware("auth:sanctum");

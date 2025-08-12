32b796218c2fc86df46b85940c73ff3645a794a4
Romain AROLDI <romain.aroldi57@gmail.com>
2025-08-12 18:26:01 +0200

    :sparkles: add contact subjects

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -37,4 +37,25 @@ class Contact extends Model
     "updated_at" => "datetime",
     "deleted_at" => "datetime",
   ];
+
+  const SUBJECT_GENERAL_INQUIRY = "general-inquiry";
+  const SUBJECT_TECHNICAL_SUPPORT = "technical-support";
+  const SUBJECT_ACCOUNT_ISSUE = "account-issue";
+  const SUBJECT_BUG_REPORT = "bug-report";
+  const SUBJECT_FEEDBACK = "feedback";
+  const SUBJECT_JOB_APPLICATION = "job-application";
+  const SUBJECT_OTHER = "other";
+
+  public static function getSubjects(): array
+  {
+    return [
+      self::SUBJECT_GENERAL_INQUIRY,
+      self::SUBJECT_TECHNICAL_SUPPORT,
+      self::SUBJECT_ACCOUNT_ISSUE,
+      self::SUBJECT_BUG_REPORT,
+      self::SUBJECT_FEEDBACK,
+      self::SUBJECT_JOB_APPLICATION,
+      self::SUBJECT_OTHER,
+    ];
+  }
 }

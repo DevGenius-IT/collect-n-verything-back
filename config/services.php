b4001ce7471e129c740b4d2980727ef4f4423e31
Romain AROLDI <romain.aroldi57@gmail.com>
2025-09-24 14:25:08 +0200

    :coffin: Remove minio (unused)

--- config/services.php
+++ config/services.php
@@ -23,12 +23,6 @@ return [
     'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
   ],
 
-  "ses" => [
-    "key" => env("AWS_ACCESS_KEY_ID"),
-    "secret" => env("AWS_SECRET_ACCESS_KEY"),
-    "region" => env("AWS_DEFAULT_REGION", "us-east-1"),
-  ],
-
   "resend" => [
     "key" => env("RESEND_KEY"),
   ],
